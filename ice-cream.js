3a98fda1f417d3ee1db6061b2946e8d84a4f1f8d
Hongjin Zhu <summerellaooo@gmail.com>
2021-02-20 09:47:50 +0000

    prints out the ingredients for making banana bread.

---
+++ ice-cream.js
@@ -0,0 +1,7 @@
+const seasons = ["spring", "summer", "fall"];
+const length = seasons.length;
+
+seasons.push("winter");
+
+console.log(length);
+console.log(seasons.length);
--- odd.js
+++ odd.js
@@ -1,5 +1,22 @@
-const isOdd = function(num){
-    return num % 2 !== 0;
+function calculateRectangleArea(length, width){
+  if (length > 0 && width > 0){
+    return length * width;
+  }else if(length < 0 && width < 0){
+    return "undefined";
+  }
 }
-console.log("3 is odd: " + isOdd(3));
-console.log("8 is odd: " + isOdd(8));
+function calculateTriangleArea(base, height){
+  if (base > 0 && height > 0){
+    return base * height / 2;
+  }else if (base < 0 && height < 0){
+    return "undefined";
+  }
+}
+function calculateCircleArea(radius){
+  if (radius < 0){
+    return "undefined";
+  } else if (radius > 0) {
+    return Math.PI * radius * radius;
+  }
+}
+calculateTriangleArea(5, -8)
